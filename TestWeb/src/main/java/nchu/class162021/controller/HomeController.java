6cd7de58eff2d7fd48f7d1eea04a75aa5e8de856
S16202128 <1304542866@qq.com>
2019-06-17 08:59:02 +0800

    yuchao

--- TestWeb/src/main/java/nchu/class162021/controller/HomeController.java
+++ TestWeb/src/main/java/nchu/class162021/controller/HomeController.java
@@ -1,8 +1,39 @@
 package nchu.class162021.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import nchu.class162021.service.QuestionService;
+
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private QuestionService qs;
+	
+	@GetMapping("/")
+	public String CheckApplyTable(Model model) {
+		try {
+			List<String> list = qs.RequestQuestion();
+		    model.addAttribute("question",list);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+	
+		return "Main";
+	}
+	
+	
+	
+	
+	
 }
---
+++ TestWeb/src/main/java/nchu/class162021/service/QuestionService.java
@@ -0,0 +1,37 @@
+package nchu.class162021.service;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+
+	public List<String> RequestQuestion() throws IOException {
+		
+		File file = new File("question.txt");
+		FileInputStream fis = new FileInputStream(file);
+		DataInputStream dis = new DataInputStream(fis);
+		BufferedReader d  = new BufferedReader(new InputStreamReader(fis));
+		List<String> list = new ArrayList<String>();
+		
+		String str;
+		while((str = d.readLine())!=null) {
+			list.add(str);
+			System.out.println(str);	
+		}
+		return list;
+	}
+	
+}
